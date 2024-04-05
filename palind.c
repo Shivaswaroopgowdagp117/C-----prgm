1b3d76b809065901f089e47858d19e03a65bde66
SHIVA SWAROOP GOWDA G P <143502801+Shivaswaroopgowdagp117@users.noreply.github.com>
2024-04-05 14:54:33 +0000

    Commit

--- palind.c
+++ palind.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <ctype.h>
+
+bool is_palindrome(char *str)
+{
+    int len = strlen(str);
+    for(int i=0;i<len/2;i++)
+    {
+        char to_lower=str[i];
+        char to_lower=str(len-1-i)
+    }
+}
